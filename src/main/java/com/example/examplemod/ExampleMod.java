c7170bbe778eb268a7d4982aa485d2d81afaf421
Hovo <avagyanh150@example.com>
2026-09-05 12:08:38 +0400

    neoforge #3 end

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -63,6 +63,7 @@ public class ExampleMod {
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
             event.accept(ModItems.AZURITE);
+            event.accept(ModItems.RAW_AZURITE);
         }
 
     }
