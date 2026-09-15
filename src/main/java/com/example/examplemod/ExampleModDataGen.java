a68075ef66c37d74d69cbbde2ec3d39eb1fce20e
Hovo-Avagyan <avagyanh150@gmail.com>
2026-09-15 12:11:28 +0400

    neoforge #6 final

--- src/main/java/com/example/examplemod/ExampleModDataGen.java
+++ src/main/java/com/example/examplemod/ExampleModDataGen.java
@@ -1,11 +1,18 @@
 package com.example.examplemod;
 
+import com.example.examplemod.datagen.ModBlockLootTableProvider;
+import com.example.examplemod.datagen.ModBlockTagsProvider;
 import com.example.examplemod.datagen.ModModelProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.Collections;
+import java.util.List;
 
 @EventBusSubscriber(modid = ExampleMod.MODID)
 
@@ -14,8 +21,12 @@ public class ExampleModDataGen {
 public static void gatherClientData(GatherDataEvent.Client event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
+        var lookupProvider = event.getLookupProvider();
 
         generator.addProvider(true, new ModModelProvider(packOutput));
+        generator.addProvider(true, new ModBlockTagsProvider(packOutput, lookupProvider));
+        generator.addProvider(true, new LootTableProvider(packOutput, Collections.emptySet(),
+                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
 
 }
 }
